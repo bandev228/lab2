ece84bb800942665d1676aedfcb33de495d40277
bandev228 <95304205+bandev228@users.noreply.github.com>
2024-09-08 22:05:58 +0700

    Hello

--- App.js
+++ App.js
@@ -1,107 +1,146 @@
-// App.js
-
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Switch, TouchableOpacity } from 'react-native';
-import { Entypo } from '@expo/vector-icons';  // Import Entypo icons
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, Vibration} from 'react-native';
+import {useState} from 'react';
+import { Entypo } from '@expo/vector-icons';
 
 export default function App() {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
-  const [input, setInput] = useState('');
-  const [result, setResult] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
+  const [currentNumber, setCurrentNumber] = useState('');
+  const [lastNumber, setLastNumber] = useState('');
 
-  const handleButtonPress = (value) => {
-    if (value === '=') {
-      calculateResult();
-    } else if (value === 'C') {
-      setInput('');
-      setResult('');
-    } else {
-      setInput(input + value);
+  const buttons = ['C', 'DEL', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '=']
+
+  function calculator() {
+    
+    let lastArr = currentNumber[currentNumber.length-1];
+    
+    if(lastArr === '/' || lastArr === '*' || lastArr === '-' || lastArr === '+' || lastArr === '.') {
+      setCurrentNumber(currentNumber)
+      return
     }
-  };
-
-  const calculateResult = () => {
-    try {
-      setResult(eval(input).toString());
-    } catch (e) {
-      setResult('Error');
+    else {
+      let result = eval(currentNumber).toString();
+      setCurrentNumber(result)
+      return
     }
-  };
+  }
 
-  return (
-    <View style={[styles.container, isDarkTheme ? styles.darkContainer : styles.lightContainer]}>
-      {/* Theme Toggle */}
-      <View style={styles.themeToggle}>
-        <Text style={styles.themeText}>{isDarkTheme ? 'Dark Theme' : 'Light Theme'}</Text>
-        <Switch value={isDarkTheme} onValueChange={setIsDarkTheme} />
+  function handleInput(buttonPressed) {
+    if(buttonPressed  === '+' || buttonPressed === '-' || buttonPressed === '*' || buttonPressed === '/') {
+      Vibration.vibrate(35);
+      setCurrentNumber(currentNumber + buttonPressed)
+      return
+    }
+    else if (buttonPressed === 1 || buttonPressed === 2 || buttonPressed === 3 || buttonPressed === 4 || buttonPressed === 5 ||
+            buttonPressed === 6 || buttonPressed === 7 || buttonPressed === 8 || buttonPressed === 9 || buttonPressed === 0 || buttonPressed === '.' ) {
+      Vibration.vibrate(35);
+    }
+    switch(buttonPressed) {
+      case 'DEL':
+        Vibration.vibrate(35);
+        setCurrentNumber(currentNumber.substring(0, (currentNumber.length - 1)))
+        return
+      case 'C':
+        Vibration.vibrate(35);
+        setLastNumber('')
+        setCurrentNumber('')
+        return
+      case '=':
+        Vibration.vibrate(35);
+        setLastNumber(currentNumber + '=')
+        calculator()
+        return
+    }
+    setCurrentNumber(currentNumber + buttonPressed)
+  }
+
+  const styles = StyleSheet.create({
+    results: {
+      backgroundColor: darkMode ? '#282f3b' : '#f5f5f5',
+      maxWidth: '100%',
+      minHeight: '35%',
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
+    },
+    resultText: {
+      maxHeight: 45,
+      color: '#00b9d6',
+      margin: 15,
+      fontSize: 35,
+    },
+    historyText: {
+      color: darkMode ? '#B5B7BB' : '#7c7c7c',
+      fontSize: 20,
+      marginRight: 10,
+      alignSelf: 'flex-end',
+    },
+    themeButton: {
+      alignSelf: 'flex-start',
+      bottom: '5%',
+      margin: 15,
+      backgroundColor: darkMode ? '#7b8084' : '#e5e5e5',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 50,
+      height: 50,
+      borderRadius: 25,
+    },
+    buttons: {
+      width: '100%',
+      height: '35%',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    button: {
+      borderColor: darkMode ? '#3f4d5b' : '#e5e5e5',
+      alignItems: 'center',
+      justifyContent: 'center',
+      minWidth: '24%',
+      minHeight: '54%',
+      flex: 2,
+    },
+    textButton: {
+      color: darkMode ? '#b5b7bb' : '#7c7c7c',
+      fontSize: 28,
+    }
+  })
+
+  return(
+    <View>
+      <View style={styles.results}>
+        <TouchableOpacity style={styles.themeButton}>
+          <Entypo name={darkMode ? 'light-up' : 'moon'} size={24} color={darkMode ? 'white' : 'black'} onPress={() => darkMode ? setDarkMode(false) : setDarkMode(true)}/>
+        </TouchableOpacity>
+        <Text style={styles.historyText}>{lastNumber}</Text>
+        <Text style={styles.resultText}>{currentNumber}</Text>
       </View>
-
-      {/* Display */}
-      <View style={styles.display}>
-        <Text style={styles.inputText}>{input}</Text>
-        <Text style={styles.resultText}>{result}</Text>
-      </View>
-
-      {/* Calculator Buttons */}
-      <View style={styles.buttonContainer}>
-        {['1', '2', '3', '+', '4', '5', '6', '-', '7', '8', '9', '*', 'C', '0', '=', '/'].map((button) => (
-          <TouchableOpacity key={button} style={styles.button} onPress={() => handleButtonPress(button)}>
-            <Text style={styles.buttonText}>{button}</Text>
+      <View style={styles.buttons}>
+        {buttons.map((button) =>
+          button === '=' || button === '/' || button === '*' || button === '-' || button === '+' ?
+          <TouchableOpacity key={button} style={[styles.button, {backgroundColor: '#00b9d6'} ]} onPress={() => handleInput(button)}>
+            <Text style={[styles.textButton, {color: 'white', fontSize: 28} ]}>{button}</Text>
           </TouchableOpacity>
-        ))}
+          :
+          button === 0 ?
+          <TouchableOpacity key={button} style={[styles.button, {backgroundColor: typeof(button) === 'number' ? darkMode ? '#303946' : '#fff' : darkMode === true ? '#414853' : '#ededed', minWidth: '36%'} ]} onPress={() => handleInput(button)}>
+            <Text style={styles.textButton}>{button}</Text>
+          </TouchableOpacity>
+          :
+          button === '.' || button === 'DEL' ?
+          <TouchableOpacity key={button} style={[styles.button, {backgroundColor: button === '.' ? darkMode ? '#303946' : '#fff' : darkMode === true ? '#414853' : '#ededed', minWidth: '37%'} ]} onPress={() => handleInput(button)}>
+            <Text style={styles.textButton}>{button}</Text>
+          </TouchableOpacity>
+          :
+          button === 'C' ?
+          <TouchableOpacity key={button} style={[styles.button, {backgroundColor: typeof(button) === 'number' ? darkMode ? '#303946' : '#fff' : darkMode === true ? '#414853' : '#ededed', minWidth: '36%'} ]} onPress={() => handleInput(button)}>
+            <Text style={styles.textButton}>{button}</Text>
+          </TouchableOpacity>
+          :
+          <TouchableOpacity key={button} style={[styles.button, {backgroundColor: typeof(button) === 'number' ? darkMode ? '#303946' : '#fff' : darkMode === true ? '#414853' : '#ededed' } ]} onPress={() => handleInput(button)}>
+            <Text style={styles.textButton}>{button}</Text>
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  lightContainer: {
-    backgroundColor: '#fff',
-  },
-  darkContainer: {
-    backgroundColor: '#333',
-  },
-  themeToggle: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginVertical: 20,
-  },
-  themeText: {
-    fontSize: 18,
-  },
-  display: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-  },
-  inputText: {
-    fontSize: 36,
-    color: '#000',
-  },
-  resultText: {
-    fontSize: 24,
-    color: '#888',
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-  },
-  button: {
-    width: '22%',
-    height: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f0f0f0',
-    marginVertical: 10,
-  },
-  buttonText: {
-    fontSize: 24,
-    color: '#000',
-  },
-});
